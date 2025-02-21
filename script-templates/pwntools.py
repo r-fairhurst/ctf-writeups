c217156da99194c1d72c7bc0327dc2372923c4ab
Ryan F <rfairhur@gmail.com>
2025-02-20 17:08:14 -0800

    better pwntools template based on what I use

--- script-templates/pwntools.py
+++ script-templates/pwntools.py
@@ -1,30 +1,29 @@
 #!/usr/bin/env python3
 from pwn import *
 
-target_os = 'linux'
-target_arch = 'amd64'
+# Define variables for the target binary and remote server
 host = 'ip'
 port = 1337
+
 executable = './vulnerable_binary'
 
-# Set up pwntools context
-context(os=target_os, arch=target_arch)
-# Define variables for the target binary and remote server
 
 def exploit():
-    io = remote(host, port)  # Connect to the remote server
-    # io = process(executable)  # Run the binary locally
+    p = remote(host, port)  # Connect to the remote server
+    # p = process(executable)  # Run the binary
 
-    io.recvuntil('')
+    p.recvuntil('')
 
     payload = b'A' * 64  # Example payload
-    payload += p64(0xaaaaaaaa)  # Example payload
+    payload += p64(0xaaaaaaaa)
 
     # Send the payload
-    io.sendline(payload)
+    p.sendline(payload)
 
     # Interact with the process
-    io.interactive()
+    p.interactive()
 
+
+# auto run the exploit function
 if __name__ == '__main__':
     exploit()
